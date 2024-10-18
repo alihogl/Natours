c636d6fdb9efa62a64b2d14dc3ce0838c8e28a55
Ali Huseyinoglu <alihoglu7@hotmail.com>
2024-10-19 02:52:04 +0300

    Some settings for production

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose.connect(DB).then(() => {
   console.log('DB connection succesfull');
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
